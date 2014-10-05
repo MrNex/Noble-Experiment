4f540deebda1a672b60c43ff97f463f77dcccab0
Andre Belanger <andrebelanger@me.com>
2014-10-05 15:31:59 -0400

    Added HUD class with dummy healthbar.

---
+++ src/Engine/HUD.java
@@ -0,0 +1,37 @@
+package Engine;
+import java.awt.Color;
+import java.awt.List;
+import java.util.ArrayList;
+import java.awt.geom.Rectangle2D;
+
+import Objects.GameObject;
+import Engine.Directory;
+
+public class HUD {
+	//private List<GameObject> hudObjects= new ArrayList<GameObject>();
+	public GameObject healthBar;
+	
+	public HUD() {
+		init();
+	}
+	
+	public void init()
+	{
+		initHealthBar();
+	}
+	
+	private void initHealthBar()
+	{
+		healthBar = new GameObject(5,15,250,25);
+		healthBar.setShape(new Rectangle2D.Double());
+		healthBar.setColor(Color.green);
+		healthBar.setVisible(true);
+		//hudObjects.add(healthBar);
+	}
+	
+	// Update HealthBar based on player health
+	private void updateHealth()
+	{
+		//healthBar. = Directory.ProfileManager.Player.getCurrentHealth() * 10;
+	}
+}
