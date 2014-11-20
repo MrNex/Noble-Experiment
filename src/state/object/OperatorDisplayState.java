328073d91a1f2b322aa8117e8a89cde812ebe6c2
Andre Belanger <andrebelanger@me.com>
2014-11-19 21:55:52 -0500

    Operation Amounts are now being displaying on operator cards in battle state.

--- src/state/object/OperatorDisplayState.java
+++ src/state/object/OperatorDisplayState.java
@@ -2,6 +2,7 @@ package state.object;
 import interfaces.Delegate;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 
 import engine.Directory;
@@ -12,6 +13,10 @@ abstract public class OperatorDisplayState extends ObjectState implements Delega
 	static protected int maxFill = 25;
 	static private Color fillColor = Color.green;
 	static private Color fontColor = Color.white;
+	
+	static private int defaultFontSize = 24;
+    static private Font defaultFont = new Font("Serif", Font.PLAIN, defaultFontSize);
+
 	
 	
 	//Attributes
@@ -67,12 +72,36 @@ abstract public class OperatorDisplayState extends ObjectState implements Delega
 		return((double)currentOps / (double)maxFill)*attachedTo.getHeight();
 		
 	}
+	
+	private int getCurrentOps(){
+		int currentOps;
+		
+		try{
+			currentOps = invoke();
+		}
+		catch(Exception e){
+			currentOps = 0;
+		}
+		
+		//If greater than max, set to max
+		if(currentOps > maxFill) currentOps = maxFill;
+		
+		return currentOps;
+		
+	}
 
 	@Override
 	public void draw(Graphics2D g2d) {
-		g2d.setColor(OperatorDisplayState.fillColor);
-		g2d.fillRect((int)(attachedTo.getXPos()), (int)(attachedTo.getYPos() + attachedTo.getHeight() - height), (int)width, (int)height);
-		
+		//g2d.setColor(OperatorDisplayState.fillColor);
+		//g2d.fillRect((int)(attachedTo.getXPos()), (int)(attachedTo.getYPos() + attachedTo.getHeight() - height), (int)width, (int)height);
+		Font save = g2d.getFont();
+		g2d.setFont(defaultFont);
+		g2d.setColor(fontColor);
+		g2d.drawString(getCurrentOps() + "", 
+				(int)(attachedTo.getXPos() + attachedTo.getWidth() / 2.0 + 8), 
+				(int)(attachedTo.getYPos() + attachedTo.getHeight() / 2.0 - 20));
+
+		g2d.setFont(save);
 	}
 
 	@Override
